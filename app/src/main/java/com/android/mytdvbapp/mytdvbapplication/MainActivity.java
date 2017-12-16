35104a84f4ea52b6186cb85b93f3a7dd86f768d9
Antoine Pelletier <pelletier.antoinepro@gmail.com>
2017-12-16 03:33:51 +0100

    Initial commit, add base for the project

--- app/src/main/java/com/android/mytdvbapp/mytdvbapplication/MainActivity.java
+++ app/src/main/java/com/android/mytdvbapp/mytdvbapplication/MainActivity.java
@@ -2,12 +2,36 @@ package com.android.mytdvbapp.mytdvbapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.mytdvbapp.mytdvbapplication.base.AbstractActivity;
+
+public class MainActivity extends AbstractActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public String getTitleBarTitle() {
+        return getResources().getString(R.string.app_name);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+    }
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        super.setContentView(view, params);
     }
 }
--- app/src/main/java/com/android/mytdvbapp/mytdvbapplication/base/AbstractActivity.java
+++ app/src/main/java/com/android/mytdvbapp/mytdvbapplication/base/AbstractActivity.java
@@ -1,15 +1,134 @@
 package com.android.mytdvbapp.mytdvbapplication.base;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.android.mytdvbapp.mytdvbapplication.AccountActivity;
+import com.android.mytdvbapp.mytdvbapplication.R;
 
 /**
  * Created by antoinepelletier on 04/12/2017.
  */
 
-public class AbstractActivity extends AppCompatActivity {
+public abstract class AbstractActivity extends AppCompatActivity implements MenuItem.OnMenuItemClickListener {
 
     private static final String TAG = "AbstractActivity";
 
+    private FrameLayout view_stub; //This is the framelayout to keep your content view
+    private NavigationView navigation_view; // The new navigation view from Android Design Library. Can inflate menu resources. Easy
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private Menu drawerMenu;
+    private ImageView mImgAccount;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_base_layout);// The base layout that contains your navigation drawer.
+        view_stub = findViewById(R.id.view_stub);
+        navigation_view = findViewById(R.id.navigation_view);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, 0, 0);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        drawerMenu = navigation_view.getMenu();
+        for (int i = 0; i < drawerMenu.size(); i++) {
+            drawerMenu.getItem(i).setOnMenuItemClickListener(this);
+        }
+
+        View header = LayoutInflater.from(this).inflate(R.layout.nav_header, null);
+        navigation_view.addHeaderView(header);
+        mImgAccount = header.findViewById(R.id.img_account);
+
+        initListener();
+    }
+
+    private void initListener() {
+        mImgAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), AccountActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /* Override all setContentView methods to put the content view to the FrameLayout view_stub
+     * so that, we can make other activity implementations looks like normal activity subclasses.
+     */
+    @Override
+    public void setContentView(int layoutResID) {
+        if (view_stub != null) {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT);
+            View stubView = inflater.inflate(layoutResID, view_stub, false);
+            view_stub.addView(stubView, lp);
+        }
+    }
+
+    @Override
+    public void setContentView(View view) {
+        if (view_stub != null) {
+            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT);
+            view_stub.addView(view, lp);
+        }
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        if (view_stub != null) {
+            view_stub.addView(view, params);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+
+        }
+        return false;
+    }
+
+    public abstract String getTitleBarTitle();
 }
